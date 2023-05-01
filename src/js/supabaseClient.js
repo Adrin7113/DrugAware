1673de413f72bd27c64d89151bb9088b5c8e3ddb
Advaith Narayanan <55801439+Glitchyi@users.noreply.github.com>
2023-05-02 01:23:30 +0530

    Delete src/js directory

--- src/js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://tbdjoduckjgzgfmbtyvs.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiZGpvZHVja2pnemdmbWJ0eXZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI0ODQzMzUsImV4cCI6MTk5ODA2MDMzNX0.GVZeuwjn_UE7HYLu7e_C9CuEpez6pudx1GtMNbMgDKU"
-);
